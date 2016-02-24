34cb6a2224192c9c2dede19e9edca3881ef2d7da
Migum <mightytomcios@gmail.com>
2016-02-24 20:56:22 +0100

    s

--- VTK-C3D.Net/VTK-C3D.Net/Form1.cs
+++ VTK-C3D.Net/VTK-C3D.Net/Form1.cs
@@ -12,6 +12,7 @@ namespace VTK_C3D.Net
 {
     public partial class Form1 : Form
     {
+        //Zmienne potrzebne do obsługi plików
         Kitware.VTK.vtkSimplePointsReader reader;
         Kitware.VTK.vtkPolyDataMapper mapper;
         Kitware.VTK.vtkRenderWindow renderwindow;
@@ -19,10 +20,10 @@ namespace VTK_C3D.Net
         Kitware.VTK.vtkActor actor;
         C3D.C3DFile file;
         System.IO.FileStream filexyz;
-        public Form1()
-        {
-            InitializeComponent();
-        }
+
+
+        //Funkcje dopisane
+        //Funkcja aktualizacji okna(Do poprawy oraz dodanie animacji).
         private void aktualizacja()
         {
 
@@ -49,43 +50,53 @@ namespace VTK_C3D.Net
             renderWindowControl1.Refresh();
 
         }
-
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Odpalenie wyszukiwania pliku
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
         }
-
+        //Udane otwarcie pliku
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             try
             {
+                //Wczytuję plik C3D
                 file = C3D.C3DFile.LoadFromFile(openFileDialog1.InitialDirectory + openFileDialog1.FileName);
+                //Kod od chińczyka inaczej nie działa.
                 UInt16 firstFrameIndex = file.Header.FirstFrameIndex;
                 UInt16 pointCount = file.Parameters["POINT:USED"].GetData<UInt16>();
                 String allframes = "";
                 String xyzstring = "";
+
                 for (Int32 i = 0; i < file.AllFrames.Count; i++)
                 {
                     //you can also use file.AllFrames[i].Point3Ds.Length
                     for (Int32 j = 0; j < pointCount; j++)
                     {
+                        //Tworzenie linii  z koordynatów punktów.
                         String line = "Frame " +
                             (firstFrameIndex + i).ToString() +
                             " " +
                             " X = " +
-                            file.AllFrames.Get3DPoint(i, j).X.ToString() +
+                            file.AllFrames.Get3DPoint(i, j).X.ToString("R") +
                             " Y = " +
-                            file.AllFrames.Get3DPoint(i, j).Y +
+                            file.AllFrames.Get3DPoint(i, j).Y.ToString() +
                             " Z = " +
-                            file.AllFrames.Get3DPoint(i, j).Z +"\n";
+                            file.AllFrames.Get3DPoint(i, j).Z.ToString() + "\n";
                         if (i == 0)
                         {
-                            String xyzline = file.AllFrames.Get3DPoint(i, j).X.ToString() + " " +
-                                file.AllFrames.Get3DPoint(i, j).Y.ToString() + " " +
-                                file.AllFrames.Get3DPoint(i, j).Z.ToString() + "\n";
-                            
+                            //Zapis pierwszej klatki do pliku XYZ
+                            String xyzline = file.AllFrames.Get3DPoint(i, j).X.ToString("G", System.Globalization.CultureInfo.CreateSpecificCulture("en-GB")) + " " +
+                                file.AllFrames.Get3DPoint(i, j).Y.ToString("G", System.Globalization.CultureInfo.CreateSpecificCulture("en-GB")) + " " +
+                                file.AllFrames.Get3DPoint(i, j).Z.ToString("G", System.Globalization.CultureInfo.CreateSpecificCulture("en-GB")) + "\n";
+
                             xyzstring = xyzstring + xyzline;
                         }
+                        //Zapis wygenerowanej linii
                         allframes = allframes + line;
                         /*Console.WriteLine("Frame {0} : X = {1}, Y = {2}, Z = {3}",
                             firstFrameIndex + i,
@@ -95,10 +106,12 @@ namespace VTK_C3D.Net
                          */
                     }
                 }
-               
+
+               //Zapis pliku XYZ
                 filexyz = System.IO.File.Create(openFileDialog1.InitialDirectory + openFileDialog1.FileName+".xyz");
                 filexyz.Write(Encoding.ASCII.GetBytes(xyzstring), 0, xyzstring.Length);
                 filexyz.Close();
+                //Wyświetlenie zawartości klatek.
                 richTextBox1.Text = allframes;
             }
             catch(Exception ex)

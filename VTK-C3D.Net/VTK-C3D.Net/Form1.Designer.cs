40f3d14e986d09c8661dd911c564336600048926
Migum <mightytomcios@gmail.com>
2016-02-24 19:53:58 +0100

    Pierwsza wersja

---
+++ VTK-C3D.Net/VTK-C3D.Net/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace VTK_C3D.Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.renderWindowControl1 = new Kitware.VTK.RenderWindowControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wybierz plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 369);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // renderWindowControl1
+            // 
+            this.renderWindowControl1.AddTestActors = false;
+            this.renderWindowControl1.Location = new System.Drawing.Point(352, 42);
+            this.renderWindowControl1.Name = "renderWindowControl1";
+            this.renderWindowControl1.Size = new System.Drawing.Size(400, 369);
+            this.renderWindowControl1.TabIndex = 2;
+            this.renderWindowControl1.TestText = null;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(352, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Wyświetl";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 492);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.renderWindowControl1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "VTK-C3D EDITOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Kitware.VTK.RenderWindowControl renderWindowControl1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VTK-C3D.Net/VTK-C3D.Net/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VTK_C3D.Net
+{
+    public partial class Form1 : Form
+    {
+        Kitware.VTK.vtkSimplePointsReader reader;
+        Kitware.VTK.vtkPolyDataMapper mapper;
+        Kitware.VTK.vtkRenderWindow renderwindow;
+        Kitware.VTK.vtkRenderer renderer;
+        Kitware.VTK.vtkActor actor;
+        C3D.C3DFile file;
+        System.IO.FileStream filexyz;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void aktualizacja()
+        {
+
+            renderWindowControl1.Refresh();
+
+            reader = new Kitware.VTK.vtkSimplePointsReader();
+            reader.SetFileName(openFileDialog1.InitialDirectory + openFileDialog1.FileName+".xyz");
+            reader.Update();
+
+            mapper = Kitware.VTK.vtkPolyDataMapper.New();
+            mapper.SetInputConnection(reader.GetOutputPort());
+            actor = Kitware.VTK.vtkActor.New();
+            actor.SetMapper(mapper);
+            actor.SetScale(0.01, 0.01, 0.01);
+            actor.GetProperty().SetPointSize(1);
+            actor.SetOrigin(0, 0, 0);
+
+
+            renderwindow = renderWindowControl1.RenderWindow;
+            renderer = renderwindow.GetRenderers().GetFirstRenderer();
+            renderer.SetBackground(0.3, 0.6, 0.3);
+            renderer.AddActor(actor);
+            renderer.Clear();
+            renderWindowControl1.Refresh();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                file = C3D.C3DFile.LoadFromFile(openFileDialog1.InitialDirectory + openFileDialog1.FileName);
+                UInt16 firstFrameIndex = file.Header.FirstFrameIndex;
+                UInt16 pointCount = file.Parameters["POINT:USED"].GetData<UInt16>();
+                String allframes = "";
+                String xyzstring = "";
+                for (Int32 i = 0; i < file.AllFrames.Count; i++)
+                {
+                    //you can also use file.AllFrames[i].Point3Ds.Length
+                    for (Int32 j = 0; j < pointCount; j++)
+                    {
+                        String line = "Frame " +
+                            (firstFrameIndex + i).ToString() +
+                            " " +
+                            " X = " +
+                            file.AllFrames.Get3DPoint(i, j).X.ToString() +
+                            " Y = " +
+                            file.AllFrames.Get3DPoint(i, j).Y +
+                            " Z = " +
+                            file.AllFrames.Get3DPoint(i, j).Z +"\n";
+                        if (i == 0)
+                        {
+                            String xyzline = file.AllFrames.Get3DPoint(i, j).X.ToString() + " " +
+                                file.AllFrames.Get3DPoint(i, j).Y.ToString() + " " +
+                                file.AllFrames.Get3DPoint(i, j).Z.ToString() + "\n";
+                            
+                            xyzstring = xyzstring + xyzline;
+                        }
+                        allframes = allframes + line;
+                        /*Console.WriteLine("Frame {0} : X = {1}, Y = {2}, Z = {3}",
+                            firstFrameIndex + i,
+                            file.AllFrames.Get3DPoint(i, j).X,
+                            file.AllFrames.Get3DPoint(i, j).Y,
+                            file.AllFrames.Get3DPoint(i, j).Z);
+                         */
+                    }
+                }
+               
+                filexyz = System.IO.File.Create(openFileDialog1.InitialDirectory + openFileDialog1.FileName+".xyz");
+                filexyz.Write(Encoding.ASCII.GetBytes(xyzstring), 0, xyzstring.Length);
+                filexyz.Close();
+                richTextBox1.Text = allframes;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            aktualizacja();
+        }
+
+        private void process1_Exited(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
